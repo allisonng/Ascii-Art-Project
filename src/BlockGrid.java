7610223bb7733ed2010f1d8d76c6a79ca2741bec
Allison Ng <aln.ng3@gmail.com>
2013-11-18 01:14:37 -0800

    Nothing much yet, just started tinkering with BufferedImage to get brightness of pixel

---
+++ src/BlockGrid.java
@@ -0,0 +1,39 @@
+
+
+import java.awt.image.BufferedImage;
+
+
+public class BlockGrid {
+
+	/* This is where the image will be subsampled.
+	 * Should also include method for finding brightness.
+	 */
+	
+	BufferedImage img;
+	
+	public BlockGrid(){
+		 /*need image width, height and type
+		 it seems BufferedImage is usually instantiated through .getSubImage or another class,
+		 and never actually new BufferImage();
+		 
+		 ImageIO class as well may be useful
+		 */
+		
+		// Filled in with random parems for now
+		img = new BufferedImage(300, 300, BufferedImage.TYPE_CUSTOM);
+		
+		// Given image, get rgb value to determine brightness at coord(x,y).
+		// getRGB() returns back an unsigned integer in form 0xAARRGGBB
+		// more info here: http://stackoverflow.com/questions/2534116/how-to-convert-get-rgbx-y-integer-pixel-to-colorr-g-b-a-in-java
+		int rgbVal = img.getRGB(1, 1);
+		int r = (rgbVal)&0xFF;
+		int g = (rgbVal>>8)&0xFF;
+		int b = (rgbVal>>16)&0xFF;
+		
+		
+	}
+
+	
+	
+	
+}
