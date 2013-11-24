b0fbdf035a83d7584b5f1e2af4b97d5e3c3f4457
Allison Ng <aln.ng3@gmail.com>
2013-11-23 22:35:48 -0800

    Before I do any class division. Cleaned up code, added comments, changed brightness matrix from float[][] to double[][].

--- src/CharacterProcess.java
+++ src/CharacterProcess.java
@@ -6,8 +6,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 
 import javax.imageio.ImageIO;
 
@@ -22,17 +20,8 @@ public class CharacterProcess {
 	Font font;
 	
 	public CharacterProcess(){
-		//img = new BufferedImage(300, 300, BufferedImage.TYPE_CUSTOM);
 	}
-	
 
-	
-	/*
-	 * Given a character set's bitmap, want to find brightness of every character.
-	 * After, initializing this, want to have a way to match image pixel's brightness 
-	 * to character's image brightness.
-	 */	
-	
 	public void createDefaultAscii(){
 		// Make sure to use only given ASCII values [32, 126]
 		int start = 32;
@@ -47,9 +36,6 @@ public class CharacterProcess {
 		
 	}
 	
-	/* Grab image.
-	 * Convert into subblocks.
-	 */
 	public BufferedImage getImage() throws IOException{
 
 		// NOT in ../src/ but in ../!!! GODDAMNIT
@@ -59,39 +45,35 @@ public class CharacterProcess {
 		return newImage;
 	}
 	
-	public float[][] convertToBlocks(BufferedImage bImg){
+	public double[][] convertToBlocks(BufferedImage bImg){
 		/*
 		 * Divide the given picture into a grid of subblocks.
 		 * Each subblock will be 4x4 pixels, and each block will be converted into a character.
+		 * 
+		 * TO BE CHANGED WITH OTHER CONVERSION ALGORITHSM: blockWidth + blockHeight
 		 */
 		int blockWidth = 4;
 		int blockHeight = 4;
+		
 		int height = bImg.getHeight();
 		int width = bImg.getWidth();
-		int rowSize = (int)Math.floor(bImg.getWidth()/4);
-		int colSize = (int)Math.floor(bImg.getHeight()/4);
-		float[][] brightnessMatrix = new float[rowSize][colSize];
 		
-		// This will grab us our subimage.
-		// Pass the subimage into method that grabs brightness
+		int rowSize = (int)Math.floor(bImg.getWidth()/blockWidth);
+		int colSize = (int)Math.floor(bImg.getHeight()/blockHeight);
+		// Changed float to double; float holds 4 bytes, double holds 8 bytes;
+		// A little excessive but more precision is always better just in case
+		double[][] brightnessMatrix = new double[rowSize][colSize];
+		
 		int row = 0;
 		int col;
 		for(int y=0; (y+blockHeight)<=bImg.getHeight(); y+=blockHeight){
-//			System.out.print("row " + y + "   col");
-			
+
 			col = 0;
 			for(int x=0; (x+blockWidth)<=bImg.getWidth(); x+=blockWidth){
-				if(x == 504){
-					String str = "hello world";
-				}
-				brightnessMatrix[row][col] 
-						= getBrightnessOfBlock(bImg.getSubimage(x, y,blockWidth, blockHeight));
-//				System.out.print(brightnessMatrix[y][x] + " ");
+				brightnessMatrix[row][col] = getBrightnessOfBlock(bImg.getSubimage(x, y,blockWidth, blockHeight));
 				col++;
-//				System.out.print(" " + x);
 			}
 			row++;
-//			System.out.print("\n");
 		}
 		
 		return brightnessMatrix;
@@ -99,18 +81,9 @@ public class CharacterProcess {
 	}
 	
 
-	public float getBrightnessOfBlock(BufferedImage bImg){
-		 /*need image width, height and type
-		 it seems BufferedImage is usually instantiated through .getSubImage or another class,
-		 and never actually new BufferImage();
-		 
-		 ImageIO class as well may be useful
-		 */
-		
-		// Filled in with random parems for now
-		
-		// Given image, get rgb value to determine brightness at coord(x,y).
-		// getRGB() returns back an unsigned integer in form 0xAARRGGBB
+	public double getBrightnessOfBlock(BufferedImage bImg){
+		// Given SUBIMAGE, get rgb value to determine brightness at coord(x,y).
+		// NOTE: getRGB() returns back an unsigned integer in form 0xAARRGGBB
 		// more info here: http://stackoverflow.com/questions/2534116/how-to-convert-get-rgbx-y-integer-pixel-to-colorr-g-b-a-in-java
 		
 		
@@ -118,7 +91,9 @@ public class CharacterProcess {
 		// Then get the average of all pixels.
 		int blockHeight = bImg.getHeight();
 		int blockWidth = bImg.getWidth();
-		float totalRgbVal = 0;
+		double totalRgbVal = 0;
+		
+		// luminosity y' = 0.299*r + 0.587*g +  0.114*b, y' = totalRgbVal in this case
 		
 		for(int y=0; y<blockHeight; y++){
 			for(int x=0; x<blockWidth; x++){
@@ -126,11 +101,12 @@ public class CharacterProcess {
 				int r = (rgbVal)&0xFF;
 				int g = (rgbVal>>8)&0xFF;
 				int b = (rgbVal>>16)&0xFF;
-				totalRgbVal = totalRgbVal + ((r+g+b)/3);
+//				totalRgbVal = totalRgbVal + ((r+g+b)/3);
+				totalRgbVal = totalRgbVal + (((0.299*r)+(0.587*g)+(0.114*b))/3);
 			}
 		}
 		
-		float blockAverage = (totalRgbVal)/(blockHeight*blockWidth);
+		double blockAverage = (totalRgbVal)/(blockHeight*blockWidth);
 		
 		return blockAverage;
 		
@@ -138,13 +114,12 @@ public class CharacterProcess {
 	}
 	
 	
-	// Now to translate from given brightnesses of image to character
 	public void convertImageToAscii(float[][] brightnessImage) throws IOException{
 		// brightnessImage represents the brightness of the image from the divided subimages
 
 		int rowHeight = brightnessImage.length;
 		int colHeight = brightnessImage[0].length;
-		System.out.println("rowheight: " + rowHeight + "colheight" + colHeight);
+
 		// Create new file to write to
 		File file = new File("asciiArt.txt");
 		if(!file.exists()){
@@ -161,7 +136,6 @@ public class CharacterProcess {
 			buffWriter.write("\n");
 		}
 		
-//		buffWriter.write();
 		buffWriter.close();
 		
 		
@@ -169,9 +143,10 @@ public class CharacterProcess {
 	
 	public char matchToChar(float brightness){
 		// Brightness is the calculated average for a subimage.
-		// That entire subimage gets converted to a char based on the brightness.
+		// That entire subimage gets converted to a char below, based on the brightness.
 		
-		if(brightness >=100){
+		// For black and white
+		if(brightness < 100){
 			return '#';
 		}
 		else{
